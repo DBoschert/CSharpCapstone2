d8f88b381a96c9aec8a3655ced821e34438fc028
Drew Boschert <drew.r.boschert@gmail.com>
2023-08-31 10:51:19 -0400

    Add project files.

---
+++ CSharpCapstone/Data/CSharpCapstoneContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CSharpCapstone.Models;
+
+namespace CSharpCapstone.Data
+{
+    public class CSharpCapstoneContext : DbContext
+    {
+        public CSharpCapstoneContext (DbContextOptions<CSharpCapstoneContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CSharpCapstone.Models.User> Users { get; set; } = default!;
+
+        public DbSet<CSharpCapstone.Models.Vendor> Vendors { get; set; } = default!;
+
+        public DbSet<CSharpCapstone.Models.Product> Products { get; set; } = default!;
+
+        public DbSet<CSharpCapstone.Models.Request> Requests { get; set; } = default!;
+
+        public DbSet<CSharpCapstone.Models.RequestLine> RequestLines { get; set; } = default!;
+    }
+}
---
+++ CSharpCapstone/Migrations/20230831142907_Added Product.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CSharpCapstone.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Vendor",
+                table: "Vendor");
+
+            migrationBuilder.RenameTable(
+                name: "Vendor",
+                newName: "Vendors");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Vendor_Code",
+                table: "Vendors",
+                newName: "IX_Vendors_Code");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Vendors",
+                table: "Vendors",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PartNbr = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(11,2)", nullable: false),
+                    Unit = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    PhotoPath = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    VendorId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Product_Vendors_VendorId",
+                        column: x => x.VendorId,
+                        principalTable: "Vendors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_PartNbr",
+                table: "Product",
+                column: "PartNbr",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_VendorId",
+                table: "Product",
+                column: "VendorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Vendors",
+                table: "Vendors");
+
+            migrationBuilder.RenameTable(
+                name: "Vendors",
+                newName: "Vendor");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Vendors_Code",
+                table: "Vendor",
+                newName: "IX_Vendor_Code");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Vendor",
+                table: "Vendor",
+                column: "Id");
+        }
+    }
+}
