03f5f848535386130a1f4d16d9d3623967ea3894
DrewBoschert <drew.r.boschert@gmail.com>
2023-11-02 17:08:49 -0400

    Made it so it works with the web

--- CSharpCapstone/Program.cs
+++ CSharpCapstone/Program.cs
@@ -2,9 +2,17 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using CSharpCapstone.Data;
 var builder = WebApplication.CreateBuilder(args);
+
+var connStrKey = "ProdDb";
+#if DOCKER
+    connStrKey = "DockerDb";
+#elif DEBUG
+connStrKey = "DevDb";
+#endif
+
 builder.Services.AddDbContext<CSharpCapstoneContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DevDb") 
-        ?? throw new InvalidOperationException("Connection string 'DevDb' not found.")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString(connStrKey)
+        ?? throw new InvalidOperationException($"Connection string {connStrKey} not found.")));
 
 // Add services to the container.
 
